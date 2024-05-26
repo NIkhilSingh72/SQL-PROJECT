175b4fb7fdd3773d43f727169267fa74891ffecd
NIkhilSingh72 <namesingh595@gmail.com>
2024-05-26 20:01:40 +0530

    Update SQL_queries(1).sql

--- SQL_queries(1).sql
+++ SQL_queries(1).sql
@@ -92,17 +92,15 @@ SELECT
     branch
 FROM sales;
 
--- --------------------------------------------------------------------
--- ---------------------------- Product -------------------------------
--- --------------------------------------------------------------------
+PRODUCT
 
--- How many unique product lines does the data have?
+ANS1
 SELECT
 	DISTINCT product_line
 FROM sales;
 
 
--- What is the most selling product line
+ANS2
 SELECT
 	SUM(quantity) as qty,
     product_line
@@ -110,7 +108,7 @@ FROM sales
 GROUP BY product_line
 ORDER BY qty DESC;
 
--- What is the most selling product line
+ANS3
 SELECT
 	SUM(quantity) as qty,
     product_line
@@ -118,7 +116,7 @@ FROM sales
 GROUP BY product_line
 ORDER BY qty DESC;
 
--- What is the total revenue by month
+ANS3
 SELECT
 	month_name AS month,
 	SUM(total) AS total_revenue
@@ -127,7 +125,7 @@ GROUP BY month_name
 ORDER BY total_revenue;
 
 
--- What month had the largest COGS?
+ANS4
 SELECT
 	month_name AS month,
 	SUM(cogs) AS cogs
@@ -136,7 +134,7 @@ GROUP BY month_name
 ORDER BY cogs;
 
 
--- What product line had the largest revenue?
+ANS5
 SELECT
 	product_line,
 	SUM(total) as total_revenue
@@ -144,7 +142,7 @@ FROM sales
 GROUP BY product_line
 ORDER BY total_revenue DESC;
 
--- What is the city with the largest revenue?
+AN6
 SELECT
 	branch,
 	city,
@@ -154,7 +152,7 @@ GROUP BY city, branch
 ORDER BY total_revenue;
 
 
--- What product line had the largest VAT?
+-ANS7
 SELECT
 	product_line,
 	AVG(tax_pct) as avg_tax
@@ -164,7 +162,7 @@ ORDER BY avg_tax DESC;
 
 
 -- Fetch each product line and add a column to those product 
--- line showing "Good", "Bad". Good if its greater than average sales
+-ANS8
 
 SELECT 
 	AVG(quantity) AS avg_qnty
@@ -180,7 +178,7 @@ FROM sales
 GROUP BY product_line;
 
 
--- Which branch sold more products than average product sold?
+ANS9
 SELECT 
 	branch, 
     SUM(quantity) AS qnty
@@ -189,7 +187,7 @@ GROUP BY branch
 HAVING SUM(quantity) > (SELECT AVG(quantity) FROM sales);
 
 
--- What is the most common product line by gender
+ANS10
 SELECT
 	gender,
     product_line,
@@ -198,153 +196,12 @@ FROM sales
 GROUP BY gender, product_line
 ORDER BY total_cnt DESC;
 
--- What is the average rating of each product line
+ANS11
 SELECT
 	ROUND(AVG(rating), 2) as avg_rating,
     product_line
 FROM sales
 GROUP BY product_line
-ORDER BY avg_rating DESC;
+ORDER BY avg_rating D
+	
 
--- --------------------------------------------------------------------
--- --------------------------------------------------------------------
-
--- --------------------------------------------------------------------
--- -------------------------- Customers -------------------------------
--- --------------------------------------------------------------------
-
--- How many unique customer types does the data have?
-SELECT
-	DISTINCT customer_type
-FROM sales;
-
--- How many unique payment methods does the data have?
-SELECT
-	DISTINCT payment
-FROM sales;
-
-
--- What is the most common customer type?
-SELECT
-	customer_type,
-	count(*) as count
-FROM sales
-GROUP BY customer_type
-ORDER BY count DESC;
-
--- Which customer type buys the most?
-SELECT
-	customer_type,
-    COUNT(*)
-FROM sales
-GROUP BY customer_type;
-
-
--- What is the gender of most of the customers?
-SELECT
-	gender,
-	COUNT(*) as gender_cnt
-FROM sales
-GROUP BY gender
-ORDER BY gender_cnt DESC;
-
--- What is the gender distribution per branch?
-SELECT
-	gender,
-	COUNT(*) as gender_cnt
-FROM sales
-WHERE branch = "C"
-GROUP BY gender
-ORDER BY gender_cnt DESC;
--- Gender per branch is more or less the same hence, I don't think has
--- an effect of the sales per branch and other factors.
-
--- Which time of the day do customers give most ratings?
-SELECT
-	time_of_day,
-	AVG(rating) AS avg_rating
-FROM sales
-GROUP BY time_of_day
-ORDER BY avg_rating DESC;
--- Looks like time of the day does not really affect the rating, its
--- more or less the same rating each time of the day.alter
-
-
--- Which time of the day do customers give most ratings per branch?
-SELECT
-	time_of_day,
-	AVG(rating) AS avg_rating
-FROM sales
-WHERE branch = "A"
-GROUP BY time_of_day
-ORDER BY avg_rating DESC;
--- Branch A and C are doing well in ratings, branch B needs to do a 
--- little more to get better ratings.
-
-
--- Which day fo the week has the best avg ratings?
-SELECT
-	day_name,
-	AVG(rating) AS avg_rating
-FROM sales
-GROUP BY day_name 
-ORDER BY avg_rating DESC;
--- Mon, Tue and Friday are the top best days for good ratings
--- why is that the case, how many sales are made on these days?
-
-
-
--- Which day of the week has the best average ratings per branch?
-SELECT 
-	day_name,
-	COUNT(day_name) total_sales
-FROM sales
-WHERE branch = "C"
-GROUP BY day_name
-ORDER BY total_sales DESC;
-
-
--- --------------------------------------------------------------------
--- --------------------------------------------------------------------
-
--- --------------------------------------------------------------------
--- ---------------------------- Sales ---------------------------------
--- --------------------------------------------------------------------
-
--- Number of sales made in each time of the day per weekday 
-SELECT
-	time_of_day,
-	COUNT(*) AS total_sales
-FROM sales
-WHERE day_name = "Sunday"
-GROUP BY time_of_day 
-ORDER BY total_sales DESC;
--- Evenings experience most sales, the stores are 
--- filled during the evening hours
-
--- Which of the customer types brings the most revenue?
-SELECT
-	customer_type,
-	SUM(total) AS total_revenue
-FROM sales
-GROUP BY customer_type
-ORDER BY total_revenue;
-
--- Which city has the largest tax/VAT percent?
-SELECT
-	city,
-    ROUND(AVG(tax_pct), 2) AS avg_tax_pct
-FROM sales
-GROUP BY city 
-ORDER BY avg_tax_pct DESC;
-
--- Which customer type pays the most in VAT?
-SELECT
-	customer_type,
-	AVG(tax_pct) AS total_tax
-FROM sales
-GROUP BY customer_type
-ORDER BY total_tax;
-
--- --------------------------------------------------------------------
--- --------------------------------------------------------------------
